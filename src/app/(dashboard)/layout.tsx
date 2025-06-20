30ac668c470bae236cd74ffdde774bce16368724
Blckops712 <seth.richardson3d@gmail.com>
2025-06-20 16:49:14 -0600

    updated layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -12,7 +12,7 @@ const DashboardLayout = async ({ children }: DashboardLayoutProps) => {
         <div className="hidden lg:block fixed left-0 top-0 lg:w-[264px] h-full overflow-y-auto">
           <Sidebar />
         </div>
-        <div className="lg:pl-[264px]">
+        <div className="lg:pl-[264px] w-full">
           <div className="mx-auto w-full max-w-screen-2xl h-full">
             <Navbar />
             <main className="h-full py-8 px-6 flex flex-col">{children}</main>
